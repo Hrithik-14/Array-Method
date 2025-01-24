79a16747abb24e98fb8cc097428a673646890ea0
Hrithik <hrithikelayur12@gmail.com>
2025-01-24 16:41:01 +0530

    first commit

---
+++ index-2.js
@@ -0,0 +1,54 @@
+function FRUITS(ARR){
+
+   
+    let fruits=ARR.at(2);
+    console.log(fruits);
+    
+}
+let ARR=[22.23,46,654,747,474,747];
+FRUITS(ARR)
+
+
+
+function food(arr){
+    let food=arr.join(",");
+    console.log(food);
+    console.log(typeof food);
+    
+}
+let array =["pizza","biriyani","mandhi","lays","shawarma"];
+food(array)
+
+
+
+function snack(arr) {
+
+    console.log(arr.pop());
+    console.log(arr);
+    
+}
+snack(arr =["1","2","3","4"]);
+
+function number(arr1){
+
+    
+    console.log(arr1.push("rf"));
+    console.log(arr1);
+    
+}
+let arr2 =["1","2","3","@","3"]
+number(arr2);
+
+
+function shiftandUnshift(arr3){
+    arr3.shift();
+    console.log(arr3);
+    
+}
+let arr3 =[1,2,34,56,67]
+shiftandUnshift(arr3)
+
+function shiftandUnshift(arr4){
+    arr4.unshift(56);
+    console.log(arr4);
+}
